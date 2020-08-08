2f8655c73aa9115377a1146af3abd4053ada06e8
Niv Haramati <niv@somoto.net>
2020-08-08 21:46:26 +0300

    done SinglyLinkedList.prototype.pop() with tests

--- singly_linked_lists_exercise/singlyLinkedList.js
+++ singly_linked_lists_exercise/singlyLinkedList.js
@@ -9,7 +9,7 @@ function SinglyLinkedList() {
     this.length = 0;
 }
 
-SinglyLinkedList.prototype.push = function (x) {
+SinglyLinkedList.prototype.push = function(x) {
     let n = new Node(x); // create a new Node
     if (this.head === null) {
         // empty list.... point both head and tail to new Node.
@@ -24,8 +24,29 @@ SinglyLinkedList.prototype.push = function (x) {
     return this; // to enable chaining
 }
 
+SinglyLinkedList.prototype.pop = function() {
+    if (this.tail === null) return undefined; // empty list. nothing to pop. leave me alone.... here, take a undefined.
+    // becasue this is a SINGLE linked list, we cannot know what is the node just before the last one...
+    // so we need to start from the head, and search for the last item to know what is one before the last
+    let lst=null;
+    let i=this.head;
+    while (i!=this.tail) {
+        lst=i;
+        i=i.next;
+    }
+    this.last=lst;
+    if (lst===null) {
+        // it was last item... set head to null too
+        this.head=null;
+    }
+    this.length--;
+    return i.val;
+}
 
 // l = new SinglyLinkedList;
-// // node = new Node(15);
-// l.push(5);
+// node = new Node(15);
+// l.push(5).push(10).push(15).push(20);
+// l.push(-3);
+// let nd=l.pop();
+// console.log(nd);
 
