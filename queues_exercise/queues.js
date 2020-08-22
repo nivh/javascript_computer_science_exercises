55b4b3f42922dd9e02da99120a80062bb3931f20
Niv Haramati <niv@somoto.net>
2020-08-22 10:11:22 +0300

    started Queue.prototype.dequeue. still not passed tests

--- queues_exercise/queues.js
+++ queues_exercise/queues.js
@@ -4,9 +4,9 @@
  * Queue constructor
  */
 function Queue() {
-    this.size=0;
-    this.first=null;
-    this.last=null;
+    this.size = 0;
+    this.first = null;
+    this.last = null;
 }
 
 /**
@@ -14,9 +14,9 @@ function Queue() {
  * @param {any} value 
  * @param {Node} next the next node (optional)
  */
-function Node(value, next=null) {
-    this.value=value;
-    this.next=next;
+function Node(value, next = null) {
+    this.value = value;
+    this.next = next;
 }
 
 /**
@@ -24,9 +24,22 @@ function Node(value, next=null) {
  * This should be an O(1) operation and return the size of the queue.
  * @param {any} value the value to add to the queue
  */
-Queue.prototype.enqueue = function(value){
-    let n=new Node(value);
-    this.last ? this.last.next=n : this.first=n;
-    this.last=n;
+Queue.prototype.enqueue = function (value) {
+    let n = new Node(value);
+    this.last ? this.last.next = n : this.first = n;
+    this.last = n;
     return ++this.size;
+}
+
+/**
+ * This function removes the value at the beginning of the queue. 
+ * This should be an O(1) operation and return the value removed.
+ */
+Queue.prototype.dequeue = function () {
+    if (this.size===0) return null; // empty Queue
+    let val=this.first.val;
+    this.first=this.first.next;
+    if (!this.first) this.last=null; // was last Node
+    this.size--;
+    return val;
 }
