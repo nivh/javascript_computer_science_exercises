6285be0852dc753c056d30cb2321c784dab77cda
Niv Haramati <niv@somoto.net>
2020-08-22 10:00:19 +0300

    Queue.prototype.enqueue done with tests

--- queues_exercise/queues.js
+++ queues_exercise/queues.js
@@ -18,3 +18,15 @@ function Node(value, next=null) {
     this.value=value;
     this.next=next;
 }
+
+/**
+ * This function adds the value to the end of the queue. 
+ * This should be an O(1) operation and return the size of the queue.
+ * @param {any} value the value to add to the queue
+ */
+Queue.prototype.enqueue = function(value){
+    let n=new Node(value);
+    this.last ? this.last.next=n : this.first=n;
+    this.last=n;
+    return ++this.size;
+}
